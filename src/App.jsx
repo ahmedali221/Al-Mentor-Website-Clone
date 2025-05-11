2e9706c7a236ef534732136d854be990971ba054
Nourhan5112001 <nourhangamal828@gmail.com>
2025-05-11 16:13:04 +0300

    Added Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,34 @@
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import LoginPage from './pages/LoginPage';
 import PasswordPage from './pages/PasswordPage';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Navbar from './components/Navbar/navbar';
+import Footer from './components/Footer/footer';
 import React from 'react';
 import SignupPage from './pages/SignupPage';
 import SignEmail from './pages/SignEmail';
-import HomePage from './pages/HomePage';
-
+import Home from './components/Home/home';
+import { ThemeProvider } from './context/ThemeContext'; 
+import './i18n/config'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <div className="min-h-[calc(100vh-128px)] py-8">
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/signup-Email" element={<SignEmail />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/password" element={<PasswordPage />} />
-        </Routes>
-      </div>
-      <Footer />
-    </BrowserRouter>
+    <ThemeProvider> {}
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[calc(100vh-128px)] py-8">
+          <Routes>
+            {/* <Route path="/" element={<LoginPage />} /> */}
+            
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/signup-Email" element={<SignEmail />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/password" element={<PasswordPage />} /> */}
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </ThemeProvider> 
   );
 }
 
