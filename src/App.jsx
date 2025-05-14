b1baf2657bc76d4bece92d2c679470b7f8908e51
RoaaYasser <roaayasser@gmail.com>
2025-05-14 10:37:51 +0300

    updating instructors

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,30 @@ import React from 'react';
 import SignupPage from './pages/SignupPage';
 import SignEmail from './pages/SignEmail';
 import Home from './components/Home/home';
+import Instructors from './pages/Instructors';
+import InstructorDetails from './pages/InstructorDetails';
 import { ThemeProvider } from './context/ThemeContext'; 
 import './i18n/config'
 import ProgramsPage from './pages/ProgramsPage';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
     <ThemeProvider> {}
       <BrowserRouter>
         <Navbar />
-        <div className="min-h-[calc(100vh-128px)] py-8">
+        <div className="min-h-[calc(100vh-128px)]">
           <Routes>
             <Route path="/loginPage" element={<LoginPage />} />
             <Route path="/programs" element={<ProgramsPage />} />
-            
             <Route path="/" element={<Home />} />
+            <Route path='/instructors' element={<Instructors />} />
+            <Route path="/instructors/:id" element={<InstructorDetails />} />
             <Route path="/signup-Email" element={<SignEmail />} />
             <Route path="/signup" element={<SignupPage />} />
             <Route path="/password" element={<PasswordPage />} />
+     
           </Routes>
         </div>
         <Footer />
